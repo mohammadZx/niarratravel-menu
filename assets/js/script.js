03e2bf0beacaddb20b4f0e198297586b2bf09ddf
یادگیری بهتر برنامه نویسی اموزش رایگان برنامه نویسی <mohammad13yazdani@gmail.com>
2021-08-09 23:33:32 +0430

    finish menu one

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,15 @@ $('.menu-one *[data-target]').click(function(){
     $('*[data-target]').removeClass('active');
     var id = $(this).attr('data-target');
     $('#' + id).toggleClass('active');
+    $('body').toggleClass('hide')
     $(this).toggleClass('clicked');
+    var imageSrc = document.querySelector('#'+id+" ul li").getAttribute('data-src');
+    $('#' + id + ' .current-image').css('background', `url(${imageSrc})`);
+    $('#' + id + ' .current-image').css('transform', `translate(0, 0)`) 
+    $('#' + id + ' .preve-image').css('background', `none`);
+
+    gsap.fromTo('#' + id + ' .img-box-hover', {opacity:0}, {opacity:1, duration:0.5, delay:1})
+    gsap.fromTo('#' + id + ' .content-box', {opacity:0}, {opacity:1, duration:0.5, delay:1})
     if(window.pageYOffset == 0){
         if($('#' + id).hasClass('active')){
             $('*[class *= menu-one] , *[class *= menu-two] , .logo-box').addClass('active');   
@@ -55,3 +63,28 @@ var currentScrollPos = window.pageYOffset;
 }
 
 
+
+
+var currentImage = null, preveImage = null, randone = {x:0,y:0}, preveRand = {x:0,y:0}
+//  image animation style
+$('.menu-items-row ul li').on('mouseover', function(){
+  var randXone = randomBetween(-50, 50), randYone = randomBetween(-50, 50);
+  preveImage = currentImage || document.querySelector('.menu-items-row ul li').getAttribute('data-src'), preveRand = randone
+  randone = {x:randXone , y:randYone }
+  var imageSrc = $(this).attr('data-src');
+
+  currentImage = imageSrc
+  
+  $('.current-image').css('background', `url(${imageSrc})`);
+  $('.preve-image').css('background', `url(${preveImage})`);
+
+  $('.current-image').css('transform', `translate(${randone.x}px, ${randone.y}px)`)
+  $('.preve-image').css('transform', `translate(${preveRand.x + 5}px, ${preveRand.y + 5}px) rotate(${randone.x / 4}deg)`)
+})
+
+
+
+function randomBetween(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+
+}
